5ddda0079a1523317c689a13f4a09a9ba1405906
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-15 22:04:14 -0300

    Remanejo da parte POO

---
+++ Java13Protected.java
@@ -0,0 +1,10 @@
+public class Java13Protected {
+    /*
+     * Metodos e atributos "protected" só podem ser usados dentro do mesmo pacote.
+     */
+    protected String test;
+
+    public Java13Protected() {
+        this.test = "Passou";
+    }
+}
